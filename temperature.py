9ae895946269723e44498aab81984c462a7b18db
georgeiiister <georgeiiister@gmail.com>
2025-01-03 17:07:38 +0300

    refactoring of code

--- temperature.py
+++ temperature.py
@@ -1,4 +1,4 @@
-from decimal import Decimal
+from decimal import Decimal as Dec
 
 class TemperatureError(Exception):
     pass
@@ -7,36 +7,37 @@ class NotSupportUnit(TemperatureError):
     pass
 
 class Temperature:
-    unit_list = {'k',
-                 'f',
-                 'c'}
+    units = {'k', 'f', 'c'}
 
-    fk = {'k2f': lambda k: Decimal(str(k)) * Decimal('1.8')
-                           - Decimal('459.67'),
-          'f2k': lambda f: (Decimal(str(f)) + Decimal('459.67'))
-                           / Decimal('1.8'),
-          'k2c': lambda k: k - Decimal('273.15'),
-          'c2k': lambda c: c + Decimal('273.15'),
-          'f2c': lambda f: (Decimal(str(f)) - Decimal('32')) / Decimal('1.8'),
-          'c2f': lambda c: Decimal(str(c))*Decimal('1.8') + Decimal('32')
-    }
+    func_key = {
+                  'k2f': lambda k: Dec(str(k)) * Dec('1.8')- Dec('459.67'),
+                  'f2k': lambda f: (Dec(str(f)) + Dec('459.67')) / Dec('1.8'),
+                  'k2c': lambda k: k - Dec('273.15'),
+                  'c2k': lambda c: c + Dec('273.15'),
+                  'f2c': lambda f: (Dec(str(f)) - Dec('32')) / Dec('1.8'),
+                  'c2f': lambda c: Dec(str(c)) * Dec('1.8') + Dec('32')
+               }
 
     @classmethod
-    def exists_unit(cls, unit, raise_exception:bool=True)->bool:
-        result:bool = unit.lower() in cls.unit_list
+    def exists_unit(cls, *unit, raise_exception:bool=True)->bool:
+        result: bool = True
+        for i in unit:
+             if i.lower() not in cls.units:
+                result = False
+                break
         if raise_exception and not result:
             raise NotSupportUnit
         return result
 
     @classmethod
     def convert(cls, value, source_unit:str, dest_unit:str):
-        Temperature.exists_unit(unit=source_unit)
-        Temperature.exists_unit(unit=dest_unit)
-        return cls.fk[f'{source_unit.lower()}2{dest_unit.lower()}'](value)
+        Temperature.exists_unit(source_unit, dest_unit)
+        func_key = f'{source_unit.lower()}2{dest_unit.lower()}'
+        return cls.fk[func_key](value) #search function converting and run
 
-    def __init__(self, value:Decimal, unit:str):
-        Temperature.exists_unit(unit = unit)
-        self.__value = Decimal(str(value))
+    def __init__(self, value:Dec, unit:str):
+        Temperature.exists_unit(unit)
+        self.__value = Dec(str(value))
         self.__unit = unit.lower()
 
     @property
@@ -54,7 +55,7 @@ class Temperature:
                                                 value = self.__value,
                                                 source_unit = self.__unit,
                                                 dest_unit = unit
-                                                )
+                                               )
             self.__unit = unit.lower()
 
     def __str__(self):
